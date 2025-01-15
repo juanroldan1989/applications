07ddf94d38b994526e652714847e97ea11022628
Juan Roldán <juanroldan1989@gmail.com>
2025-01-15 21:43:09 +0100

    services moved into `services` folder
    - Github Actions workflows adjusted accordingly
    - Build/Push script adjusted accordingly

--- scripts/build-and-push-images.sh
+++ scripts/build-and-push-images.sh
@@ -2,8 +2,8 @@
 
 for app in farewell greeting greeter fareweller name sqs-processor; do
   echo "Building Docker image for $app..."
-  cd $app
-  docker build -t <docker-hub-username>/$app .
-  docker push <docker-hub-username>/$app
+  cd services/$app
+  docker build -t juanroldan1989/$app .
+  docker push juanroldan1989/$app
   cd ..
 done
