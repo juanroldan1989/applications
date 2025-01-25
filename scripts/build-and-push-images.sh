32bfa5bcb8b190c05c270f5644ce667ca72f8967
Juan Roldan <juanroldan1989@gmail.com>
2025-01-25 13:43:46 +0100

    Scripts - Build and Push Docker images improved
    - Docker images built with multi-platform support

--- scripts/build-and-push-images.sh
+++ scripts/build-and-push-images.sh
@@ -7,7 +7,13 @@ cd ../services
 for app in farewell greeting greeter fareweller name sqs-processor; do
   echo "Building Docker image for $app..."
   cd $app
-  docker build -t juanroldan1989/$app .
+
+  # Docker images built with multi-platform support (Linux/amd64 and Linux/arm64)
+  docker buildx build --platform linux/amd64,linux/arm64 -t juanroldan1989/$app .
+  # Useful for development on Apple M1 or other ARM-based systems
+  # Useful for deployments on ARM-based systems like Raspberry Pi
+  # Useful for deployments on EKS Nodes with x86_64 (amd64) architecture
+
   docker push juanroldan1989/$app
   cd ..
 done
