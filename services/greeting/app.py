a9b07bc19a4c2f93a15ae01da39b24fcd7ce8a6e
Juan Roldán <juanroldan1989@gmail.com>
2025-01-31 20:10:47 +0100

    `Greeting` App updated
    - `health` endpoint added

--- services/greeting/app.py
+++ services/greeting/app.py
@@ -9,5 +9,9 @@ greetings = ["Hello", "Hi", "Greetings", "Salutations", "Hey"]
 def get_greeting():
   return jsonify(greeting=random.choice(greetings))
 
+@app.route('/health', methods=['GET'])
+def health():
+  return jsonify(status='healthy')
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5002)
